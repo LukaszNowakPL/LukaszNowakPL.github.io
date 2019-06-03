867ec27551aa2a73564e9f1c67e63f817aa80378
lukasz.nowak.at.phyzzi.com <lukasz.nowak@phyzzi.com>
2019-06-03 19:55:01 +0200

    Testing builded application

---
+++ precache-manifest.265d07ebc4ac584a42fb0de60cb01072.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f6cf360b4ace323a13aafe4e0ff74a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "459fbfa5de991709fcc6",
+    "url": "/static/css/main.72e26706.chunk.css"
+  },
+  {
+    "revision": "d67767c4da3f0b5d347b",
+    "url": "/static/js/2.475a8caf.chunk.js"
+  },
+  {
+    "revision": "459fbfa5de991709fcc6",
+    "url": "/static/js/main.52d90f6c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
